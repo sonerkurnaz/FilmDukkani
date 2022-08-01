03a528669477f203e1f7e8c409f9596d19d174c8
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-08-01 17:54:07 +0300

    _layout, viewstart ve imports itemları eklendi

--- FilmDukkani/Program.cs
+++ FilmDukkani/Program.cs
@@ -24,8 +24,13 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 
 app.Run();
