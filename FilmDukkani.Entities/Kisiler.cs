0b6c857040411de4424868c17e9dc1bf6275fbb4
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-08-03 18:57:50 +0300

    admin layout düzeltildi

--- FilmDukkani.Entities/Kisiler.cs
+++ FilmDukkani.Entities/Kisiler.cs
@@ -5,11 +5,12 @@
         public string Adi { get; set; }
         public string Soyadi { get; set; }
         public string TCKN { get; set; }
-        public string Mail { get; set; }
+        public string Email { get; set; }
         public string Gsm { get; set; }
         public string KrediKartNo { get; set; }
         public string KrediKartSkt { get; set; }
         public string KrediKartCvc { get; set; }
+        public string KullaniciAdi { get; set; }
         public string Sifre { get; set; }
 
 
--- FilmDukkani/Program.cs
+++ FilmDukkani/Program.cs
@@ -27,9 +27,13 @@ app.UseAuthorization();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
-      name: "areas",
-      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+      name: "default",
+      pattern: "{controller=Home}/{action=Index}/{id?}"
     );
+    endpoints.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+            );
 });
 
 
